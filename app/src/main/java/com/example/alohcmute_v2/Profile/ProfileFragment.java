f269e17c4bae96fc1efabb5bebdb6b0e886012fc
21110512@st.hcmute.edu.vn <21110512@st.hcmute.edu.vn>
2024-05-12 04:31:17 +0700

    add giao dien

---
+++ app/src/main/java/com/example/alohcmute_v2/Profile/ProfileFragment.java
@@ -0,0 +1,39 @@
+package com.example.alohcmute_v2.Profile;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.alohcmute_v2.R;
+
+public class ProfileFragment extends Fragment {
+
+    private MainViewModel mViewModel;
+
+    public static ProfileFragment newInstance() {
+        return new ProfileFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+}
