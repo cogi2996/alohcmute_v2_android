028acdb27101a019ea03138583f2d6de61322137
Van Tin <vantin22112003@gmail.com>
2024-05-12 14:35:26 +0700

    giao dien

---
+++ app/src/main/java/com/example/alohcmute_v2/ReelActivity.java
@@ -0,0 +1,14 @@
+package com.example.alohcmute_v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reel);
+    }
+}
